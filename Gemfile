55318c6b1f92012bf548359cbc2402d98363c198
mwatha <mwatha.bwanalie@baobabhealth.org>
2012-09-03 11:08:49 +0200

    removing .yml from git hub

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rails', '2.3.5'
+gem 'warden'
+gem 'devise'
+gem 'settingslogic'
+gem 'fastercsv'
+gem 'rest-client'
+gem 'mechanize'
+gem 'json', '1.5.0'
--- script/initial_database_setup.sh
+++ script/initial_database_setup.sh
@@ -22,41 +22,46 @@ if [ ! -x config/database.yml ] ; then
   cp config/database.yml.example config/database.yml
 fi
 
+sudo apt-get install wkhtmltopdf
+
 USERNAME=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['username']"`
 PASSWORD=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['password']"`
 DATABASE=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['database']"`
-HOST=`ruby -ryaml -e "puts YAML::load_file('config/database.yml')['${ENV}']['host']"`
 
-echo "DROP DATABASE $DATABASE;" | mysql --host=$HOST --user=$USERNAME --password=$PASSWORD
-echo "CREATE DATABASE $DATABASE;" | mysql --host=$HOST --user=$USERNAME --password=$PASSWORD
+echo "DROP DATABASE $DATABASE;" | mysql --user=$USERNAME --password=$PASSWORD
+echo "CREATE DATABASE $DATABASE;" | mysql --user=$USERNAME --password=$PASSWORD
 
 
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/openmrs_1_7_2_concept_server_full_db.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/openmrs_1_7_2_concept_server_full_db.sql
 #echo "schema additions"
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/schema_bart2_additions.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/bart2_views_schema_additions.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/schema_bart2_additions.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/bart2_views_schema_additions.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/openmrs_metadata_1_7.sql
 #echo "defaults"
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/defaults.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/defaults.sql
 #echo "user schema modifications"
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/user_schema_modifications.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/malawi_regions.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/mysql_functions.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/drug_ingredient.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/pharmacy.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/national_id.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/weight_for_heights.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/${SITE}.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/tasks.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/moh_regimens_only.sql
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/regimen_indexes.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/retrospective_station_entries.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/create_dde_server_connection.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/user_schema_modifications.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/malawi_regions.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/mysql_functions.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/drug_ingredient.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/pharmacy.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/national_id.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/weight_for_heights.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/${SITE}.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/data/${SITE}/tasks.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/moh_regimens_only.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/regimen_indexes.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/retrospective_station_entries.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/create_dde_server_connection.sql
 
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/privilege.sql
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/bart2_role_privileges.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/migrate/create_weight_height_for_ages.sql
-mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/migrate/insert_weight_for_ages.sql
-#mysql --host=$HOST --user=$USERNAME --password=$PASSWORD $DATABASE < db/regimens.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/privilege.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/bart2_role_privileges.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/migrate/create_weight_height_for_ages.sql
+mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/migrate/insert_weight_for_ages.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/concept.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/concept_name.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/order_type.sql
+#mysql --user=$USERNAME --password=$PASSWORD $DATABASE < db/regimens.sql
 
 #rake openmrs:bootstrap:load:defaults RAILS_ENV=$ENV
 #rake openmrs:bootstrap:load:site SITE=$SITE RAILS_ENV=production#
