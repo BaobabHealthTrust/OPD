5b017375804c3306f03628d60af02bbc57aed69c
Blessings <blessings.mhango@gmail.com>
2012-09-05 10:25:11 +0200

    configure pagination gem

--- config/environment.rb
+++ config/environment.rb
@@ -13,7 +13,7 @@ Rails::Initializer.run do |config|
 	# config.time_zone = 'UTC'
 	config.gem 'warden'
 	config.gem 'devise'
-    
+        config.gem 'will_paginate', :version => '~> 2.3.16'  
 	config.action_controller.session = {
 		:session_key => 'bart_session',
 		:secret      => '8sgdhr431ba87cfd9bea177ba3d344a67acb0f179753f37d28db8bd102134261cdb4b1dbacccb126435631686d66e148a203fac1c5d71eea6abf955a66a472ce'
