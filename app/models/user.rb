25fea0edae477e3b05a53b108053a0554abe67cc
Ernest Matola <mangochiman@gmail.com>
2012-11-23 16:09:30 +0200

    added a method to check the status of a user whether on pending, blocked or active

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,7 +40,7 @@ class User < ActiveRecord::Base
 		  :class_name => 'UserProperty',
 		  :foreign_key => :user_id,
 		  :conditions => ['property = ?', 'Activities'] 
-
+  has_one :user_activation
 	# Custom search method for our custom login attribute
 	def self.find_for_database_authentication(warden_conditions)
 		conditions = warden_conditions.dup
@@ -169,4 +169,9 @@ class User < ActiveRecord::Base
 	def self.current=(user)
 		Thread.current[:user] = user
 	end
+
+  def status
+  user_status = self.user_activation.status rescue ''
+  return user_status
+  end
 end
