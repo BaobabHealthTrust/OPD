c5938b13f0a5963ab703db43e1762feeb245a4d2
blessings <blessings.mhango@gmail.com>
2013-02-14 15:16:15 +0200

    worked on create from form method to create identifier with supplied identifier

--- lib/patient_service.rb
+++ lib/patient_service.rb
@@ -1024,7 +1024,6 @@ EOF
   
 	def self.create_from_form(params)
     return nil if params.blank?
-
 		address_params = params["addresses"]
 		names_params = params["names"]
 		patient_params = params["patient"]
@@ -1037,7 +1036,7 @@ EOF
 		elsif person_params["gender"].to_s == "Male"
       person_params["gender"] = 'M'
 		end
-   
+
 		person = Person.create(person_params)
 
 		unless birthday_params.empty?
@@ -1048,22 +1047,22 @@ EOF
 		  end
 		end
 		person.save
-	   
+
 		person.names.create(names_params)
 		person.addresses.create(address_params) unless address_params.empty? rescue nil
 
 		person.person_attributes.create(
 		  :person_attribute_type_id => PersonAttributeType.find_by_name("Occupation").person_attribute_type_id,
 		  :value => params["occupation"]) unless params["occupation"].blank? rescue nil
-	 
+
 		person.person_attributes.create(
 		  :person_attribute_type_id => PersonAttributeType.find_by_name("Cell Phone Number").person_attribute_type_id,
 		  :value => params["cell_phone_number"]) unless params["cell_phone_number"].blank? rescue nil
-	 
+
 		person.person_attributes.create(
 		  :person_attribute_type_id => PersonAttributeType.find_by_name("Office Phone Number").person_attribute_type_id,
 		  :value => params["office_phone_number"]) unless params["office_phone_number"].blank? rescue nil
-	 
+
 		person.person_attributes.create(
 		  :person_attribute_type_id => PersonAttributeType.find_by_name("Home Phone Number").person_attribute_type_id,
 		  :value => params["home_phone_number"]) unless params["home_phone_number"].blank? rescue nil
@@ -1072,19 +1071,25 @@ EOF
 
 		if (!patient_params.nil?)
 		  patient = person.create_patient
-      
+       params["identifiers"].each{|identifier_type_name, identifier|
+        next if identifier.blank?
+        identifier_type = PatientIdentifierType.find_by_name(identifier_type_name) || PatientIdentifierType.find_by_name("Unknown id")
+        patient.patient_identifiers.create("identifier" => identifier, "identifier_type" => identifier_type.patient_identifier_type_id)
+		  } if params["identifiers"]
+=begin
 		  patient_params["identifiers"].each{|identifier_type_name, identifier|
         next if identifier.blank?
         identifier_type = PatientIdentifierType.find_by_name(identifier_type_name) || PatientIdentifierType.find_by_name("Unknown id")
         patient.patient_identifiers.create("identifier" => identifier, "identifier_type" => identifier_type.patient_identifier_type_id)
 		  } if patient_params["identifiers"]
-
+=end
 		  # This might actually be a national id, but currently we wouldn't know
 		  #patient.patient_identifiers.create("identifier" => patient_params["identifier"], "identifier_type" => PatientIdentifierType.find_by_name("Unknown id")) unless params["identifier"].blank?
 		end
 
 		return person
 	end
+
 
   # Get the any BMI-related alert for this patient
   def self.current_bmi_alert(patient_weight, patient_height)
