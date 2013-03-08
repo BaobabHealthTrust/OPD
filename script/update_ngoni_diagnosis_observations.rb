073b8e88541fd59f48e9abb438906276227623d2
blessings <blessings.mhango@gmail.com>
2013-03-08 08:23:23 +0200

    update sccript

--- script/update_ngoni_diagnosis_observations.rb
+++ script/update_ngoni_diagnosis_observations.rb
@@ -3,7 +3,7 @@
 # then change all the diagnosis encounter_type to outpatient diagnosis
 
 start_time = Time.now().strftime('%Y-%m-%d %H:%M:%S')
-puts "Start Time: #{start_time}\n\n"
+puts "Start Time: #{start_time}"
 logger = Logger.new(Rails.root.join("log",'update_diagnosis_observations.log'))
 logger.info "Start Time: #{start_time}"
 total_saved = 0
@@ -11,9 +11,11 @@ total_not_saved = 0
 
 primary_diagnosis_concept_id = ConceptName.find_by_name('PRIMARY DIAGNOSIS').concept_id
 
+outpatient_diagnosis_encounter_type_id = EncounterType.find_by_name('OUTPATIENT DIAGNOSIS').encounter_type_id
+
 diagnosis_obs = Observation.find_by_sql("SELECT o.*, e.provider_id, e.form_id, e.encounter_datetime FROM encounter e
                                           LEFT JOIN obs o ON o.encounter_id = e.encounter_id
-                                          WHERE e.encounter_type = 8
+                                          WHERE e.encounter_type = #{outpatient_diagnosis_encounter_id}
                                           AND e.voided = 0
                                           AND o.voided = 0")
 
@@ -34,7 +36,9 @@ diagnosis_obs.each do |aDiagnosis|
         obs[:creator] = aDiagnosis.creator
         Observation.create(obs)
         total_saved += 1
-        logger.info "Total saved => #{total_saved} concept => #{obs[:concept_id]} value_coded => #{obs[:value_coded]} encounter => #{obs[:encounter_id]}"
+        msg = "Total saved => #{total_saved} concept => #{obs[:concept_id]} value_coded => #{obs[:value_coded]} encounter => #{obs[:encounter_id]}"
+        logger.info msg
+        puts msg
         #void the diagnosis
         aDiagnosis.voided = 1
         aDiagnosis.date_created = Date.today
@@ -48,7 +52,6 @@ end
 
 #update the diagnosis encounter_type to outpatient diagnosis encounter_type
 #
-outpatient_diagnosis_encounter_type_id = EncounterType.find_by_name('OUTPATIENT DIAGNOSIS').encounter_type_id
 diagnosis_encounter_type = EncounterType.find_by_name('DIAGNOSIS').encounter_type_id
 
 #pull out all diagnosis encounters that are not voided
