50813b6610f7e49e0c865608a2198e1ac8eebb4a
blessings <blessings.mhango@gmail.com>
2013-03-25 16:35:57 +0200

    creating proper user

--- db/create_and_alter_tables_before_openmrs_migration.sql
+++ db/create_and_alter_tables_before_openmrs_migration.sql
@@ -1,20 +1,20 @@
 #add SD role
 INSERT INTO role VALUES ('System Developer','System Developers');
 #add oligad as sd user.
-INSERT INTO user_role VALUES (16369, 'System Developer');
+INSERT INTO user_role VALUES (1, 'System Developer');
 
 #create person
 INSERT INTO person (person_id,gender, dead,creator,date_created)
-VALUES (1,'M',0,16369,NOW());
+VALUES (1,'M',0,1,NOW());
 
 #Create Person Name
 INSERT INTO person_name(person_name_id,person_id,given_name,family_name,creator)
-VALUES (1,1,'Super','User',16369);
+VALUES (1,1,'Super','User',1);
 
 #create user
-INSERT INTO users(user_id,username,password,salt,creator,date_created) VALUES (1,'admin',
+INSERT INTO users(user_id,username,password,salt,creator,date_created) VALUES (220,'admin',
                   '4a1750c8607d0fa237de36c6305715c223415189','c788c6ad82a157b712392ca695dfcf2eed193d7f',
-                  16369,NOW());
+                  1,NOW());
 
 #Add concept name tag map
 SET FOREIGN_KEY_CHECKS = 0;
