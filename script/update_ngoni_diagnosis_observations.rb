59c6f09c4b7dccbd2d3968639f5e94c58d6853fd
Blessings <blessings.mhango@gmail.com>
2013-08-17 13:54:27 +0200

    specified diagnosis observations

--- script/update_ngoni_diagnosis_observations.rb
+++ script/update_ngoni_diagnosis_observations.rb
@@ -12,42 +12,45 @@ total_not_saved = 0
 
 primary_diagnosis_concept_id = ConceptName.find_by_name('PRIMARY DIAGNOSIS').concept_id
 
+diagnosis_concept_id = ConceptName.find_by_name('DIAGNOSIS').concept_id
+
 outpatient_diagnosis_encounter_type_id = EncounterType.find_by_name('OUTPATIENT DIAGNOSIS').encounter_type_id
 
 diagnosis_obs = Observation.find_by_sql("SELECT o.*, e.provider_id, e.form_id, e.encounter_datetime FROM encounter e
                                           LEFT JOIN obs o ON o.encounter_id = e.encounter_id
                                           WHERE e.encounter_type = #{outpatient_diagnosis_encounter_type_id}
+                                          AND o.concept_id = #{diagnosis_concept_id}
                                           AND e.voided = 0
                                           AND o.voided = 0")
 
-diagnosis_obs.each do |aDiagnosis|
+diagnosis_obs.each do |diagnosis|
   
     #create the primary diagnosis
-    if aDiagnosis.value_coded
-        value_coded_name_id = ConceptName.find_by_sql("SELECT concept_name_id FROM concept_name WHERE concept_id = #{aDiagnosis.value_coded}").map{|c| c.concept_name_id}
+    if diagnosis.value_coded
+        value_coded_name_id = ConceptName.find_by_sql("SELECT concept_name_id FROM concept_name WHERE concept_id = #{diagnosis.value_coded}").map{|c| c.concept_name_id}
         #create the primary diagnosis
         obs = {}
         obs[:concept_id] = primary_diagnosis_concept_id
-        obs[:value_coded] = aDiagnosis.value_coded
+        obs[:value_coded] = diagnosis.value_coded
         obs[:value_coded_name_id] = value_coded_name_id.first
-        obs[:encounter_id] = aDiagnosis.encounter_id
-        obs[:obs_datetime] = aDiagnosis.obs_datetime
-        obs[:person_id] = aDiagnosis.person_id
-        obs[:location_id] = aDiagnosis.location_id
-        obs[:creator] = aDiagnosis.creator
+        obs[:encounter_id] = diagnosis.encounter_id
+        obs[:obs_datetime] = diagnosis.obs_datetime
+        obs[:person_id] = diagnosis.person_id
+        obs[:location_id] = diagnosis.location_id
+        obs[:creator] = diagnosis.creator
         Observation.create(obs)
         total_saved += 1
         msg = "Total saved => #{total_saved} concept => #{obs[:concept_id]} value_coded => #{obs[:value_coded]} encounter => #{obs[:encounter_id]}"
         logger.info msg
         puts msg
         #void the diagnosis
-        aDiagnosis.voided = 1
-        aDiagnosis.date_created = Date.today
-        aDiagnosis.void_reason = "Migration"
-        aDiagnosis.save
+        diagnosis.voided = 1
+        diagnosis.date_created = Date.today
+        diagnosis.void_reason = "Migration"
+        diagnosis.save!
       else
         total_not_saved += 1
-        logger.info "Total unsaved => #{total_not_saved} concept => #{aDiagnosis.concept_id} obs_id => #{aDiagnosis.obs_id} encounter => #{aDiagnosis.encounter_id}"
+        logger.info "Total unsaved => #{total_not_saved} concept => #{diagnosis.concept_id} obs_id => #{diagnosis.obs_id} encounter => #{diagnosis.encounter_id}"
       end
 end
 
@@ -58,22 +61,7 @@ weight_concept_id = ConceptName.find_by_name("Weight").concept_id
 normal_weight_obs = Observation.find(:all, :conditions =>   ["concept_id = ? ", normal_weight_concept_id])
 normal_weight_obs.each do |ob|
   ob.concept_id = weight_concept_id
-  ob.save
-end
-
-#update the diagnosis encounter_type to outpatient diagnosis encounter_type
-diagnosis_encounter_type = EncounterType.find_by_name('DIAGNOSIS').encounter_type_id
-
-#pull out all diagnosis encounters that are not voided
-diagnosis_encounters = Encounter.find_by_sql("SELECT * FROM encounter
-                                              WHERE encounter_type = #{diagnosis_encounter_type} 
-                                              AND voided = 0 ")
-
-diagnosis_encounters.each do |aDiagnosis_encounter|
-  #change the diagnosis encounter_type_id to outpatient_diagnosis encounter_type_id
-  aDiagnosis_encounter.encounter_type = outpatient_diagnosis_encounter_type_id
-  aDiagnosis_encounter.date_created = Date.today
-  aDiagnosis_encounter.save
+  ob.save!
 end
 
 end_time = Time.now().strftime('%Y-%m-%d %H:%M:%S')
