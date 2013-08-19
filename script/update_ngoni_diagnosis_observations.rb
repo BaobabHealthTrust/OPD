ec6b21286647d4ec489700f46072a52939afb735
Blessings <blessings.mhango@gmail.com>
2013-08-19 16:07:51 +0200

    included program script main update script

--- script/update_ngoni_diagnosis_observations.rb
+++ script/update_ngoni_diagnosis_observations.rb
@@ -115,6 +115,48 @@ normal_weight_obs.each do |ob|
   ob.save!
 end
 
+#adding programs to migrated patients with registered patients
+
+
+Encounter.find_by_sql("
+				SELECT * FROM encounter e
+				WHERE encounter_type = (SELECT encounter_type_id FROM encounter_type WHERE name = 'REGISTRATION')
+				AND patient_id not IN (SELECT DISTINCT(patient_id) FROM patient_program WHERE program_id = 14)").each {|patient |
+
+  current = Patient.find(patient.patient_id)
+
+				if current.patient_programs.in_programs("OPD PROGRAM").blank?
+					current = current.patient_programs.create(
+							  :program_id => 14,
+                :creator => 1,
+                :location_id => patient.location_id,
+							  :date_enrolled => patient.encounter_datetime)
+          puts "working program for patient #{patient.patient_id}"
+        end
+
+				}
+
+ concept_name = ConceptName.find_all_by_name("Following")
+
+  state = ProgramWorkflowState.find(:first, :conditions => ["concept_id IN (?)",concept_name.map{|c|c.concept_id}] ).program_workflow_state_id
+
+PatientProgram.find_by_sql("
+        SELECT * FROM patient_program
+        WHERE patient_program_id NOT IN (SELECT patient_program_id FROM patient_state)").each {|pg|
+        states = PatientState.new(
+          :patient_program_id => pg.patient_program_id,
+          :start_date => pg.date_enrolled,
+          :state => state,
+          :creator => 1
+          )
+        states.save!
+
+
+          puts "working states for patient #{pg.patient_id}"
+
+        }
+
+
 end_time = Time.now()
 logger.info "End Time : #{end_time.strftime('%Y-%m-%d %H:%M:%S')}"
 puts "End Time : #{end_time.strftime('%Y-%m-%d %H:%M:%S')}\n\n"
