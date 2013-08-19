06ef0a4e6352c2263ec1aeb6d0dc4ce1aa83792d
Blessings <blessings.mhango@gmail.com>
2013-08-19 15:02:14 +0200

    enrolling patients to OPD program

--- script/add_program_migrated_data.rb
+++ script/add_program_migrated_data.rb
@@ -3,21 +3,23 @@
 
 Encounter.find_by_sql("
 				SELECT * FROM encounter e
-				WHERE encounter_type = (SELECT encounter_type_id FROM encounter_type WHERE name = 'hiv staging')
-				AND patient_id not IN (SELECT DISTINCT(patient_id) FROM patient_program WHERE program_id = 1)").each {|patient |
-				current = Patient.find(patient.patient_id)
+				WHERE encounter_type = (SELECT encounter_type_id FROM encounter_type WHERE name = 'REGISTRATION')
+				AND patient_id not IN (SELECT DISTINCT(patient_id) FROM patient_program WHERE program_id = 14)").each {|patient |
+
+  current = Patient.find(patient.patient_id)
 				
-				if current.patient_programs.in_programs("HIV PROGRAM").blank?
+				if current.patient_programs.in_programs("OPD PROGRAM").blank?
 					current = current.patient_programs.create(
-							  :program_id => 1,
+							  :program_id => 14,
                 :creator => 1,
+                :location_id => patient.location_id,
 							  :date_enrolled => patient.encounter_datetime)
           puts "working program for patient #{patient.patient_id}"
         end
          
 				}
 
- concept_name = ConceptName.find_all_by_name("Pre-art (continue)")
+ concept_name = ConceptName.find_all_by_name("Following")
 
   state = ProgramWorkflowState.find(:first, :conditions => ["concept_id IN (?)",concept_name.map{|c|c.concept_id}] ).program_workflow_state_id
 
