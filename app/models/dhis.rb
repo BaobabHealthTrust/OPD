befc898106ae1aa0319a31c6f7146c0fc6304d5a
Catchit <precious.bondwe@baobabhealth.org>
2014-03-12 16:19:47 +0200

    removed extra joins in methods for getting both primary and secondary diagnosis

--- app/models/dhis.rb
+++ app/models/dhis.rb
@@ -43,12 +43,6 @@ class Dhis
 				concept_name dcn ON dcn.concept_id = o.value_coded AND dcn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8346
 					LEFT JOIN
 				concept_name scn ON scn.concept_id = o.value_coded AND scn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8347
-					LEFT JOIN
-				concept_name sscn ON sscn.concept_id = o.value_coded AND sscn.concept_name_id = o.value_coded_name_id AND o.concept_id = 6543
-					LEFT JOIN
-				concept_name dsscn ON dsscn.concept_id = o.value_coded AND dsscn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8346
-					LEFT JOIN
-				concept_name ssscn ON ssscn.concept_id = o.value_coded AND ssscn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8347
 			WHERE
 				o.concept_id IN (6543, 8346, 8347)
 			GROUP BY o.encounter_id
@@ -93,12 +87,6 @@ class Dhis
 				concept_name dcn ON dcn.concept_id = o.value_coded AND dcn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8345
 					LEFT JOIN
 				concept_name scn ON scn.concept_id = o.value_coded AND scn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8348
-					LEFT JOIN
-				concept_name sscn ON sscn.concept_id = o.value_coded AND sscn.concept_name_id = o.value_coded_name_id AND o.concept_id = 6543
-					LEFT JOIN
-				concept_name dsscn ON dsscn.concept_id = o.value_coded AND dsscn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8346
-					LEFT JOIN
-				concept_name ssscn ON ssscn.concept_id = o.value_coded AND ssscn.concept_name_id = o.value_coded_name_id AND o.concept_id = 8347
 			WHERE
 				o.concept_id IN (6542 , 8345, 8348)
 			GROUP BY o.encounter_id
