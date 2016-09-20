6b0f33d0a6228a2a1b089a67695a940e199537b3
pike-msonda <pike.msonda@gmail.com>
2016-09-20 11:55:03 +0200

    temperataure record migration

--- db/migrate/20160920082045_create_temperature_records.rb
+++ db/migrate/20160920082045_create_temperature_records.rb
@@ -1,7 +1,8 @@
 class CreateTemperatureRecords < ActiveRecord::Migration
   def self.up
     create_table :temperature_records do |t|
-
+      t.column :patient_identifier, :string, :limit => 35
+      t.column :temperature, :decimal
       t.timestamps
     end
   end
