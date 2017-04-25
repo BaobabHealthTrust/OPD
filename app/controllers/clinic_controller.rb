6ac6055f98137bfaabb535fe5a95f303dc5e45ec
mangochiman <mangochiman@gmail.com>
2017-04-25 16:09:47 +0200

    Added back OPD General

--- app/controllers/clinic_controller.rb
+++ app/controllers/clinic_controller.rb
@@ -48,7 +48,7 @@ class ClinicController < GenericClinicController
     session[:observation] = nil
     session[:people] = nil
     @reports = [
-      #["OPD General", "/cohort_tool/opd_report_index"],
+      ["OPD General", "/cohort_tool/opd_report_index"],
       ["Disaggregated Diagnosis", "/cohort_tool/opd_menu?report_name=disaggregated_diagnosis"],
       ["Diagnosis (By address)", "/cohort_tool/opd_menu?report_name=diagnosis_by_address"],
       #["Patient Level Data", "/cohort_tool/opd_menu?report_name=patient_level_data"],
